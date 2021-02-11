5f75aebb866de3dc4950b8b049f7166baa58d186
Mihai Chiru <mihai@greenlytics.io>
2021-02-11 20:42:01 +0100

    Auto-retry api calls when 429 statuses are received

--- rebase/util/api_request.py
+++ rebase/util/api_request.py
@@ -1,6 +1,23 @@
 import requests
 import rebase as rb
+import time
+import random
 
+def robust(max_tries=5, retry_statuses=[429]):
+    def decorator(func):
+        def handle_call(*args, **kwargs):
+            n_try = 0
+            while n_try < max_tries:
+                response = func(*args, **kwargs)
+                if response.status_code in retry_statuses:
+                    time.sleep(2**n_try * (1+random.random()))
+                    n_try += 1
+                else:
+                    return response
+        return handle_call
+    return decorator
+
+@robust(max_tries=5, retry_statuses=[429])
 def get(path, **kwargs):
     headers = {'Authorization': rb.api_key, 'GL-API-KEY': rb.api_key}
     url = rb.base_api_url+path
@@ -10,7 +27,7 @@ def get(path, **kwargs):
 
     return response
 
-
+@robust(max_tries=5, retry_statuses=[429])
 def post(path, **kwargs):
     headers = {
         'Authorization': rb.api_key,
